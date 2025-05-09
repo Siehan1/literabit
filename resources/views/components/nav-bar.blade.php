3d72c7724a5e42a3a1e33f278d26cb9ad1898e61
Achmad soewardi <soewardiachmad40@gmail.com>
2025-05-09 21:31:13 +0700

    component navbar belum selesai

---
+++ resources/views/components/nav-bar.blade.php
@@ -0,0 +1,32 @@
+<!-- <nav class="bg-primary border-gray-200 dark:bg-gray-900">
+        <div class="flex flex-wrap justify-beetween items-center mx-auto max-w-screen-xl p-4">
+            <a href="#" class="">
+                <img src="{{asset('asset/images/icon.png')}}" alt="LittleRabbit Logo" class="h-24">
+                <span>LittleRabbit</span>
+                <span>Little Reading Habbit</span>
+            </a>
+        </div>
+        <div>
+            
+        </div>
+</nav> -->
+
+<header class="flex flex-row justify-between item-center mx-7 h-20 text-teks font-poppins">
+
+    <div class="inline-flex justify-center items-center gap-2 pt-2">
+        <a href="#" class="flex item-center pt-3"><img src="{{asset('asset/images/icon.svg')}}" alt="LiteraBit Logo" class="w-14 h-max"></a>
+        <div class="w-24 inline-flex flex-col justify-start items-start">
+            <span class="self-stretch justify-start text-primary text-base font-bold font-['Poppins'] leading-tight">LittleRabbit</span>
+            <span class="LittleReadingHabit self-stretch justify-start text-orange-300 text-[10.24px] font-normal font-['Poppins'] leading-3">Little Reading Habbit</span>
+        </div>
+    </div>
+
+    <nav class="flex flex-row items-center ">
+        <ul class="flex flex-row gap-8">
+
+        </ul>
+    </nav>
+    <div class="flex flex-row gap-2 items-center">
+            <a href="#" class="bg-primary px-9 py-2 rounded-4xl font-medium transition-colors ease-in duration-200 hover:bg-hover shadow-[0px_4px_0px_0px_rgba(201,144,75,1.00)]">Login</a>
+    </div>
+</header>
