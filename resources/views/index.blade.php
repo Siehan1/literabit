1e4694dcc4645ec67cc6bdb1a4b1e4d8c1995563
Achmad soewardi <soewardiachmad40@gmail.com>
2025-05-10 08:31:01 +0700

    nambah icon web

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing Page</title>
+    <link rel="icon" href="{{ asset('asset/images/icon.svg') }}" type="image/svg+xml">
     @vite(['resources/css/app.css','resources/js/navbar.js'])
 </head>
 <body>
