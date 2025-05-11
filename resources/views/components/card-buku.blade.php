5a289581e3d1131f0cecd477ba5811043b18f7f7
Achmad soewardi <soewardiachmad40@gmail.com>
2025-05-11 21:34:24 +0700

    card done

--- resources/views/components/card-buku.blade.php
+++ resources/views/components/card-buku.blade.php
@@ -1,17 +1,17 @@
-<div class="w-52 bg-white rounded-3xl p-4 shadow-prim font-poppins">
+<div class="w-36 md:w-44 lg:w-52 bg-white rounded-3xl p-2 md:p-3 lg:p-4 shadow-prim font-poppins">
     <div class="relative">
-        <div class="absolute top-2 -right-[5.5px] z-10">
-            <img src="{{ asset('asset/images/label_buku.svg') }}" alt="label" class="w-24">
-            <span class="absolute -top-[1px] right-[5px] text-[14px]">{{$genre}}</span>
+        <div class="absolute top-2 -right-[5.5px] z-10 flex items-center">
+            <img src="{{ asset('asset/images/label_buku.svg') }}" alt="label" class="w-20 md:w-22 lg:w-24">
+            <span class="absolute text-teks w-full text-center -top-0 text-[12px] md:text-[13px] lg:text-[14px]">{{$genre}}</span>
         </div>
-        <img src="{{ asset($cover) }}" alt="buku" class="rounded-2xl w-full h-48 object-cover">
+        <img src="{{ asset($cover) }}" alt="buku" class="rounded-2xl w-full h-40 md:h-44 lg:h-48 object-cover">
     </div>
 
     <div class="mt-3">
-        <h1 class="text-xl font-semibold text-teks">{{ $judul }}</h1>
+        <h1 class="text-lg md:text-xl lg:text-xl font-semibold text-teks truncate">{{ $judul }}</h1>
         <div class="flex items-center gap-2 mt-2">
-            <img src="{{ asset($profile) }}" alt="penulis" class="w-8 h-8 rounded-full">
-            <p class="text-sm text-sec">By {{$penulis}}</p>
+            <img src="{{ asset($profile) }}" alt="penulis" class="w-6 md:w-7 lg:w-8 h-6 md:h-7 lg:h-8 rounded-full">
+            <p class="text-xs md:text-sm lg:text-sm text-sec">By {{$penulis}}</p>
         </div>
     </div>
 </div>
--- resources/views/components/navBarLP.blade.php
+++ resources/views/components/navBarLP.blade.php
@@ -1,10 +1,12 @@
-<header class="flex flex-row justify-between item-center lg:px-7 lg:pb-2 text-teks font-poppins pb-2 px-4 border-b-2 border-gray-300 fixed top-0 left-0 right-0 bg-white z-50 transition-transform duration-300">
+<header class="flex flex-row justify-between item-center px-4 md:px-6 lg:px-7 pb-2 text-teks font-poppins border-b-2 border-gray-300 fixed top-0 left-0 right-0 bg-white z-50 transition-transform duration-300">
     <!-- Logo dan Brand -->
     <div class="inline-flex justify-center items-center gap-2 pt-2">
-        <a href="#" class="flex item-center pt-3"><img src="{{asset('asset/images/icon.svg')}}" alt="LiteraBit Logo" class="lg:w-11 lg:h-max w-10"></a>
+        <a href="#" class="flex item-center pt-3">
+            <img src="{{asset('asset/images/icon.svg')}}" alt="LiteraBit Logo" class="w-10 md:w-10 lg:w-11 h-max">
+        </a>
         <div class="w-24 inline-flex flex-col justify-start items-start">
-            <span class="self-stretch justify-start text-primary text-base font-bold font-['Poppins'] leading-tight">LittleRabbit</span>
-            <span class="LittleReadingHabit self-stretch justify-start text-orange-300 text-[10.24px] font-normal font-['Poppins'] leading-3">Little Reading Habbit</span>
+            <span class="self-stretch justify-start text-primary text-sm md:text-base font-bold font-['Poppins'] leading-tight">LittleRabbit</span>
+            <span class="self-stretch justify-start text-orange-300 text-[9px] md:text-[10.24px] font-normal font-['Poppins'] leading-3">Little Reading Habbit</span>
         </div>
     </div>
 
@@ -17,7 +19,7 @@
             </svg>
         </button>
 
-        <ul class="flex flex-col lg:flex-row gap-8 mb-4 lg:mb-0 mt-20 lg:mt-0">
+        <ul class="flex flex-col lg:flex-row gap-6 md:gap-7 lg:gap-8 mb-4 lg:mb-0 mt-20 lg:mt-0">
             @foreach ([
                 'Beranda' => '#beranda',
                 'Buku' => '#buku',
@@ -26,7 +28,7 @@
                 'Kontak' => '#kontak'
             ] as $text => $link)
                 <li class="text-center">
-                    <a href="{{ $link }}" class="text-teks relative group px-4 py-2 block hover:text-primary transition-colors duration-200">
+                    <a href="{{ $link }}" class="text-sm md:text-base text-teks relative group px-3 md:px-4 py-2 block hover:text-primary transition-colors duration-200">
                         {{ ucfirst($text) }}
                         <div class="absolute w-full h-0.5 bg-primary scale-x-0 group-hover:scale-x-100 transition-transform left-0 mt-1 group-active:scale-x-100"></div>
                     </a>
@@ -36,8 +38,8 @@
     </nav>
 
     <!-- Tombol Login dan Hamburger -->
-    <div class="flex flex-row gap-3 items-center">
-        <a href="#" class="bg-primary lg:px-9 lg:py-2 px-4 py-1.5 rounded-4xl font-medium transition-colors ease-in duration-200 hover:bg-hover shadow-[0px_4px_0px_0px_rgba(201,144,75,1.00)]">Login</a>
+    <div class="flex flex-row gap-2 md:gap-3 items-center">
+        <a href="#" class="bg-primary px-3 md:px-6 lg:px-9 py-1.5 md:py-2 rounded-4xl text-sm md:text-base font-medium transition-colors ease-in duration-200 hover:bg-hover shadow-[0px_4px_0px_0px_rgba(201,144,75,1.00)]">Login</a>
         <button class="group lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3" class="lg:hidden block fill-teks group-hover:fill-primary transition-colors duration-200">
                 <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>
