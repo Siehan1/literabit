6551b5f95d081708c7b8a3126a586a8b159a912c
Achmad soewardi <soewardiachmad40@gmail.com>
2025-05-16 20:11:14 +0700

    login done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function beranda()
     {
         $users = User::all();
         return view('beranda.beranda',compact('users'));
