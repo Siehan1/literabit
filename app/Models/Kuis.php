41c705384bf47ccec50140eb830547e3d813292b
Achmad soewardi <soewardiachmad40@gmail.com>
2025-05-21 15:58:33 +0700

    admin panel 80 persen jadi tinggal ud nya aja

--- app/Models/Kuis.php
+++ app/Models/Kuis.php
@@ -7,32 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kuis extends Model
 {
-    use HasFactory;
-
-    protected $fillable = [
-        'buku_id', // Tambahkan baris ini
-        'pertanyaan',
-        // ... kolom lain yang diizinkan diisi massal ...
-    ];
-
-    // Atau jika Anda menggunakan $guarded dan ingin mengizinkan semua kecuali beberapa:
-    // protected $guarded = []; // Array kosong berarti tidak ada yang dijaga (semua fillable)
-
-    public function book()
-    {
-        return $this->belongsTo(Buku::class, 'buku_id');
-    }
-
-    public function lesson()
-    {
-        return $this->belongsTo(ReadingLesson::class, 'lesson_id');
-    }
-
-    /**
-     * Get the choices for the Kuis.
-     */
-    public function choices()
-    {
-        return $this->hasMany(Choice::class);
-    }
+    //
 }
