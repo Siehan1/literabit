85efedc13328d458eec2124a40c4e5e17db6fede
Achmad soewardi <soewardiachmad40@gmail.com>
2025-05-22 20:08:04 +0700

    mission template tinggal ud

---
+++ resources/views/admin/mision/tableMision.blade.php
@@ -0,0 +1,30 @@
+<x-dashboardComponent.admin>
+<x-slot name="header">Template Missions</x-slot>
+    @if(session('success'))
+        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mb-4 flex flex-row gap-1.5">
+            <i class="bi bi-check-circle-fill"></i>
+            <span>{{ session('success') }}</span>
+            
+        </div>
+    @endif
+    <div class="w-full text-end mb-6">
+        <a href="{{ route('uploadTemplate') }}" class="bg-secondary-600 px-4 py-3 rounded-2xl hover:bg-secondary-800 text-white transition-colors duration-100">Tambah Buku</a>
+    </div>
+    <x-dashboardComponent.table>
+        <x-slot name="thead">
+            <th class="px-6 py-3 text-left text-[14px] font-semibold uppercase w-1/6">Tipe</th>
+            <th class="px-6 py-3 text-left text-[14px] font-semibold uppercase w-1/6">Deskripsi</th>
+            <th class="px-6 py-3 text-left text-[14px] font-semibold uppercase w-1/6">Jumlah Targe</th>
+            <th class="px-6 py-3 text-left text-[14px] font-semibold uppercase w-1/6">XP Reward</th>
+            @foreach ($template as $t)
+            <tr>
+                <td class="px-5 py-3 text-left w-1/6">{{ $t->type }}</td>
+                <td class="px-5 py-3 text-left w-1/6">{{ $t->deskripsi }}</td>
+                <td class="px-5 py-3 text-left w-1/6">{{ $t->jumlah_target }}</td>
+                <td class="px-5 py-3 text-left w-1/6">{{ $t->xp_reward }}</td>
+            </tr>
+            @endforeach
+        </x-slot>
+
+    </x-dashboardComponent.table>
+</x-dashboardComponent.admin>
